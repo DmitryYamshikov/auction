a6d76564b3d7bcbb214f8851069c70c67016e9ae
Dmitry <DmitryYamshikov@gmail.com>
2020-09-16 21:27:17 +0700

    complete block promo

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,8 +8,8 @@ window.addEventListener("DOMContentLoaded", () => {
         menu.classList.remove("active");
         subMenu.classList.toggle("active");
     });
-    document.addEventListener('keydown', (e)=>{
-        if (e.code == 'Escape') {
+    document.addEventListener("keydown", (e) => {
+        if (e.code == "Escape") {
             subMenu.classList.remove("active");
         }
     });
@@ -17,8 +17,39 @@ window.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.querySelector(".header__hamburger");
     const menu = document.querySelector(".header__menu");
 
-    hamburger.addEventListener('click', ()=>{
-        menu.classList.toggle('active');
+    hamburger.addEventListener("click", () => {
+        menu.classList.toggle("active");
         subMenu.classList.remove("active");
-    })
+    });
+
+    //? slider
+    const slides = document.querySelectorAll(".promo__slider_item");
+    const prev = document.querySelector(".slider-nav__arrow_prev");
+    const next = document.querySelector(".slider-nav__arrow_next");
+
+    let slideIndex = 1;
+
+    showSlides(1);
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach((item) => (item.style.display = "none"));
+        slides[slideIndex - 1].style.display = "";
+    }
+
+    function plusSlides(n) {
+        showSlides((slideIndex += n));
+    }
+
+    prev.addEventListener("click", () => {
+        plusSlides(-1);
+    });
+    next.addEventListener("click", ()=>{
+        plusSlides(1);
+    });
 });
